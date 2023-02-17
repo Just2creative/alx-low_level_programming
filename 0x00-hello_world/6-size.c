77c177a395fe9d1eaa48aa4cc304eed884283f43
MichaelObiekwe <michaelobiekwe86@gmail.com>
2023-02-17 08:05:45 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program printing sizes of computer
+ * main - program printing sizes of computer types
  * return 0 (success)
  */
 int main(void)
@@ -9,13 +9,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(e));
-
+printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 return (0);
 }
