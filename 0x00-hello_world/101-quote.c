55fdd8d2c1c46cc57f9e2d193ee9360b28b28dd5
MichaelObiekwe <michaelobiekwe86@gmail.com>
2023-02-17 08:31:07 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints to standard error
+ * return 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
