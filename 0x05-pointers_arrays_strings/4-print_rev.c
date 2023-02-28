0d85942c18af3771a321cf9453eab16cbc72c05a
MichaelObiekwe <michaelobiekwe86@gmail.com>
2023-02-27 20:36:41 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main'h"
+/**
+ * print_rev - a function to print string in reverse
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
