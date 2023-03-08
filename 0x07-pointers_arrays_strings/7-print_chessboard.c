737ba6f5750936485ad95162ebbb421259f30976
MichaelObiekwe <michaelobiekwe86@gmail.com>
2023-03-08 11:51:28 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,21 +2,20 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: pointer to print
- * Return: 0 successful
+ * @a: pointer to pieces to print
+ *
+ * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
-
 	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-	{
-		_ putchar(a[i][j]);
-	}
-		_putchar("\n");
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
